94fa44a9dcb65bec7ade1a3931c0392e36f573f8
Bert Brunekreeft <BBrunekreeft@gmail.com>
2024-01-17 16:45:14 +0100

    fix: use proper config settings

--- src/Controllers/PaynlRedirectController.php
+++ src/Controllers/PaynlRedirectController.php
@@ -2,6 +2,7 @@
 
 namespace Lobbesnl\Lunar\Paynl\Controllers;
 
+use Lunar\Facades\Payments;
 use Lunar\Models\Order;
 use Lunar\Models\Transaction;
 
@@ -9,26 +10,25 @@ class PaynlRedirectController
 {
     public function redirect(Order $order, Transaction $transaction)
     {
-        // TODO: check if transaction has succeeded
         if (!$transaction->reference) {
-            return redirect()->route(config('lunar.mollie.payment_failed_route'));
+            return redirect()->route(config('lunar.paynl.payment_failed_route'));
         }
 
         // Transaction succeeded, authorize payment
-        $paymentAuthorize = Payments::driver('paynl')->withData([
-            'paymentId' => $transaction->reference,
-        ])->authorize();
+        $paymentAuthorize = Payments::driver('paynl')
+            ->withData(['paymentId' => $transaction->reference,])
+            ->authorize();
 
         if (!$paymentAuthorize->success) {
             $data = json_decode($paymentAuthorize->message, true);
 
             return match ($data['status']) {
-                'open' => redirect()->route(config('lunar.mollie.payment_open_route')),
-                'canceled' => redirect()->route(config('lunar.mollie.payment_canceled_route')),
-                default => redirect()->route(config('lunar.mollie.payment_failed_route')),
+                'open' => redirect()->route(config('lunar.paynl.payment_open_route')),
+                'canceled' => redirect()->route(config('lunar.paynl.payment_canceled_route')),
+                default => redirect()->route(config('lunar.paynl.payment_failed_route')),
             };
         }
 
-        return redirect()->route(config('lunar.mollie.payment_paid_route'));
+        return redirect()->route(config('lunar.paynl.payment_paid_route'));
     }
 }
